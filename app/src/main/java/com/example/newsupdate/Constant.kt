b0c56fc406401654a584194564b7fdea278eaacf
Mayur <cmayur.1404@gmail.com>
2024-12-10 19:03:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/newsupdate/Constant.kt
@@ -0,0 +1,6 @@
+package com.example.newsupdate
+
+object Constant {
+    //val apiKey = "12a127a13b1d4735aa058e0de7ba0e3a"
+    val apiKey = "e4e64af26dc446ccbb461708d2249f18"
+}
---
+++ app/src/main/java/com/example/newsupdate/Route.kt
@@ -0,0 +1,11 @@
+package com.example.newsupdate
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+object HomePageScreen
+@Serializable
+data class NewsArticleScreen(
+    val url :String
+
+)
